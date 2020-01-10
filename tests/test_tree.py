877a0dca58cb7d10454ed8652afff93ef3578c03
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-10 18:36:39 +0100

    Simple tree model

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,5 +1,38 @@
-def func(x):
-    return x + 1
+from yglu.tree import *
 
-def test_answer():
-    assert func(3) == 4
+
+def test_constant():
+    m = Mapping()
+    m['a'] = Scalar(1)
+    assert m['a'] == 1
+
+    s = Sequence()
+    s.append(Scalar('hello'))
+    assert s[0] == 'hello'
+
+
+def test_content():
+    class OneNode(Node):
+        version = 0
+
+        def create_content(self):
+            self.version += 1
+            return self.version
+
+    n = OneNode()
+    s = Sequence()
+    s.append(n)
+    s.append(n)
+    m2 = Mapping()
+    m2['s'] = s
+    m = Mapping()
+    m['s'] = s
+    m['n'] = n
+    m['m2'] = m2
+
+    assert m['s'][0] == 1
+    assert m['s'][1] == 1
+    assert m['n'] == 1
+    assert m['m2']['s'] == [1, 1]
+
+    assert m2.content() == {'s': [1, 1]}
--- yglu/tree.py
+++ yglu/tree.py
@@ -1,29 +1,54 @@
-"""
+'''
 Model for the internal document structure.
-Collections accessors triggers the content creation of sub-nodes.
-"""
+Node content can be the result of a computation.
+'''
 from collections import OrderedDict
 
-class Node:
-    """ All elements of the tree are nodes"""
-    visible = True 
-    memo = None
-    def content(self):
-        if(memo is None)
-            memo = self.create_content()
-        return memo
 
-class Scalar:
+class Node:
+    visible = True
+    memo = None
+
+    def content(self):
+        if(self.memo is None):
+            self.memo = self.create_content()
+        return self.memo
+
+    def create_content(self):
+        return self
+
+
+class Scalar(Node):
     def __init__(self, value):
         self.memo = value
 
+
 class Mapping(Node, OrderedDict):
     def get_node(self, key):
-        return OrderedDict.__getitem__(self, key)
-    def __getitem__(self, key):
-        return OrderedDict.__getitem__(self, key).content()
-    def __setitem__(self, key, value):
-        
+        return super().__getitem__(key)
 
-class Sequence():
-    pass
+    def __getitem__(self, key):
+        return super().__getitem__(key).content()
+
+    def __items__(self):
+        return self.content().__items__()
+
+    def create_content(self):
+        return {k: self[k] for k in self.keys()}
+
+
+class Sequence(list, Node):
+    def get_node(self, index):
+        return super().__getitem__(self, index)
+
+    def __getitem__(self, index):
+        return super().__getitem__(index).content()
+
+    def __iter__(self):
+        return self.content().__iter__()
+
+    def __eq__(self, value):
+        return self.content.__eq__(value)
+
+    def create_content(self):
+        return [self[i] for i in range(len(self))]
