7d79bb9b2f70e20705dc0f81e0612552810d7fcb
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-11 00:16:55 +0100

    Cycle detection in expression evaluation

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -4,7 +4,7 @@ from yglu.tree import *
 
 
 def test_execution():
-    set_scope({'a': 1})
+    init({'a': 1})
     assert ExpressionNode("$.a + 1").content() == 2
 
 
@@ -13,7 +13,7 @@ def test_simple_reference():
     s = Sequence()
     m['a'] = Scalar(2)
     m['b'] = ExpressionNode('$.a + 1')
-    set_scope(m)
+    init(m)
     assert m['b'] == 3
 
 
@@ -24,7 +24,7 @@ def test_sequence_references():
     s.append(ExpressionNode('$.s[1] + 1'))
     s.append(Scalar(2))
     m['a'] = ExpressionNode('$.s[0] + 1')
-    set_scope(m)
+    init(m)
     assert m['a'] == 4
     assert m == { 's': [3, 2], 'a': 4 }
 
@@ -36,6 +36,20 @@ def test_multiple_references():
     s.append(Scalar(2))
     m['a'] = ExpressionNode('$.s[0] + 1')
     m['b'] = ExpressionNode('$["a"] * 2')
-    set_scope(m)
+    init(m)
     assert m['b'] == 8
     assert m == {'s': [3, 2], 'b': 8, 'a': 4}
+
+def test_circular_reference():
+    m = Mapping()
+    s = Sequence()
+    m['s'] = s
+    s.append(ExpressionNode('$.s[1]'))
+    s.append(ExpressionNode('$.b'))
+    m['b'] = ExpressionNode('$.s[0]')
+    init(m)
+    try:
+        m['b']
+    except CircularReferenceException:
+        return
+    assert False
--- yglu/expression.py
+++ yglu/expression.py
@@ -6,14 +6,25 @@ engine = yaql.factory.YaqlFactory().create(options={
     'yaql.convertInputData': False
 })
 scope = {}
+stack = []
 
-def set_scope(new_scope):
+def init(new_scope):
     global scope
+    global stack
     scope = new_scope
+    stack = []
 
 class ExpressionNode(Node):    
     def __init__(self, expression):
         self.expression = expression
 
     def create_content(self):
-        return engine(self.expression).evaluate(data=scope)
+        if self in stack:
+            raise CircularReferenceException()
+        stack.append(self)
+        result = engine(self.expression).evaluate(data=scope)
+        stack.pop()
+        return result
+
+class CircularReferenceException(Exception):
+    pass
