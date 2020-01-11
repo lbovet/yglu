d3665f7f452821080d99c2801e39579c9257dc45
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-11 20:09:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ylgu-lbovet',
     version='0.1',
-    description='YAML Glue',
+    description='YAML glue for structural templating and processing',
     author='Laurent Bovet',
     author_email='laurent.bovet@windmaster.ch',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lbovet/yglu",
     packages=["yglu"],
     entry_points={
-        "console_scripts": ["yglu=yglu.cli:main"]},
+        "console_scripts": ["yglu=yglu.cli:main"]
+    },
     install_requires=[
         "ruamel.yaml",
         "yaql"
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
