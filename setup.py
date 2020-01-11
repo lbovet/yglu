f9c993a6d629135a32694ab8dcdc97e85180f049
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-11 20:40:38 +0100

    PyPi deploy config

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("yglu/__init__.py", "r") as fh:
+    exec(fh.read())
+
 setup(
     name='ylgu-lbovet',
-    version='0.1',
+    version=version,
     description='YAML glue for structural templating and processing',
     author='Laurent Bovet',
     author_email='laurent.bovet@windmaster.ch',
@@ -17,12 +20,14 @@ setup(
         "console_scripts": ["yglu=yglu.cli:main"]
     },
     install_requires=[
-        "ruamel.yaml",
-        "yaql"
+        "ruamel.yaml==0.16.5",
+        "yaql==1.1.3"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
