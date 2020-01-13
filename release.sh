e5aede53bf2a68867796632616e7581e05e4aa35
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-13 11:48:06 +0100

    Release script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pytest
+if [ $? -eq 0 ]; then
+    echo "version='"$1"'" > yglu/__init__.py
+    git add .
+    git commit -m"Release v$1"
+    git push
+    git tag v$1
+    git push --tags
+fi
