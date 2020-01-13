4a9e19835f05e4b9e113c9d04a5e2a5ab35e2fd2
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-13 12:00:19 +0100

    Fix commit message

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ pytest
 if [ $? -eq 0 ]; then
     echo "version='"$1"'" > yglu/__init__.py
     git add .
-    git commit -m"Release v$1"
+    git commit -m"Release $1"
     git push
     git tag v$1
     git push --tags
