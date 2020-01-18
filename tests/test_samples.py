c37ee39477f5b99adecc705d54c89e090d1c815a
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-18 16:14:09 +0100

    Sample tests as parametrized tests

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,17 +1,22 @@
 import os
 import glob
+import pytest
 from yglu.dumper import dump
 from .utils import *
 
 
+def simple_name(filename):
+    return os.path.splitext(os.path.basename(filename))[0]
+
 path = os.path.dirname(os.path.realpath(__file__))
-filenames = [f for f in glob.glob(path + "/samples/**/*.yml", recursive=True)]
+files = [pytest.param(f, id=simple_name(f))
+         for f in glob.glob(path + "/samples/**/*.yml", recursive=True)]
+
+os.environ['YGLU_ENABLE_ENV'] = 'true'
 
 
-def test_samples():
-    os.environ['YGLU_ENABLE_ENV'] = 'true'
-    for filename in filenames:
-        print("Sample: "+filename)
-        with open(filename) as file:
-            (input, output) = process(file, filename)
-            assert_like(input, output)
+@pytest.mark.parametrize("filename", files)
+def test_sample(filename):
+    with open(filename) as file_handle:
+        (input, output) = process(file_handle, filename)
+        assert_like(input, output)
