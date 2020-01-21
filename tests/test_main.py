2da43007c5175b56069e4e98846a606b5292a5dc
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-21 22:51:49 +0100

    Test multi doc

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,17 @@
 from yglu.main import process
 from io import StringIO
-
+from .utils import outdent
 
 def test_process_ok():
     input = '''
         a: 1 
         b: 2
+        ---
+        c: 3
     '''
     output = StringIO()
-    process(input, output)
-    output.flush()
-    assert output.getvalue() == 'a: 1\nb: 2\n'
+    process(outdent(input), output)
+    assert output.getvalue() == 'a: 1\nb: 2\n---\nc: 3\n'
 
 
 def test_process_failure():
